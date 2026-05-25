cce2d00180b4d0bda6ddb83c6d5702349f833e9a
lifuyuan <lifuyuan33@gmail.com>
2026-05-25 12:57:05 +0800

    上门取货的订单，创建任务时第1、2条线路需要合并在一起; 创建任务时，可以指定多辆卡车

--- app/controllers/order_routes_controller.rb
+++ app/controllers/order_routes_controller.rb
@@ -2,8 +2,32 @@ class OrderRoutesController < ApplicationController
   before_action :auth_staff
   
   def index
-    @all_routes = OrderRoute.all
-    @pagy, @routes = pagy(:offset, @all_routes.order("id desc"))
+    @all_routes = OrderRoute.where(status: ["init", "rearranged"])
+    # 上门取货的订单，创建任务时第1、2条线路需要合并在一起
+    @routes = [] # 单个线路
+    @c_routes = [] # 合并线路
+    c_ids = []
+    @all_routes.order("id asc").each do |route|
+      next if c_ids.include? route.id
+      if route.order.need_pickup && route.node_order == 1
+        if route2 = @all_routes.find_by(order_id: route.order_id, node_order: 2)
+          c_ids << route.id << route2.id
+          @c_routes << [route, route2]
+        else
+          @routes << route
+        end
+      elsif route.order.need_pickup && route.node_order == 2
+        if route1 = @all_routes.find_by(order_id: route.order_id, node_order: 1)
+          c_ids << route.id << route1.id
+          @c_routes << [route1, route]
+        else
+          @routes << route
+        end
+      else
+        @routes << route
+      end
+    end
+    #@pagy, @routes = pagy(:offset, @all_routes.order("id desc"))
   end
 
 	def destroy
--- app/controllers/transport_tasks_controller.rb
+++ app/controllers/transport_tasks_controller.rb
@@ -9,7 +9,16 @@ class TransportTasksController < ApplicationController
 	def new
 		begin
 			raise "Please Select Routes to Create Tasks" if params[:ids].blank?
-			@ids = params[:ids]
+			@ids = []
+			params[:ids].each do |ids|
+				if ids.include? ",,,"
+					ids.split(",,,").each do |sid|
+						@ids << sid
+					end
+				else
+					@ids << ids
+				end
+			end
 			@task = TransportTask.new
 		rescue=>e
 			flash[:error] = e.message
@@ -23,26 +32,34 @@ class TransportTasksController < ApplicationController
       flash[:error] = "Please Select Routes to Create Tasks"
       redirect_to order_routes_path and return
     end
-		@task = TransportTask.new(params.require(:transport_task)
-      .permit(:truck_id, :task_date))
+		
     ActiveRecord::Base.transaction do
       begin
-	      @task.order_quantity = @ids.count
-        @task.save!
-	      @ids.each do |route_id| 
+      	@ids.each do |route_id| 
 	      	route = OrderRoute.find(route_id)
 	      	if route.status!="init" && route.status!="rearranged"
 	      		raise "Can't arrange this route, status=#{route.status}"
 	      	end
-	      	@tor = TaskOrderRelation.new(
-	      		task_id: @task.id,
-	      		order_route_id: route.id,
-	      		status: "init"
-	      		)
-	      	@tor.save!
-	      	route.status="scheduled"
-	      	route.save!
 	      end
+      	params.require(:transport_task).permit(truck_id: [])["truck_id"].each do |truck_id|
+      		if truck_id.present?
+	      		@task = TransportTask.new(params.require(:transport_task).permit(:task_date))
+	      		@task.truck_id = truck_id
+			      @task.order_quantity = @ids.count
+		        @task.save!
+			      @ids.each do |route_id| 
+			      	route = OrderRoute.find(route_id)
+			      	@tor = TaskOrderRelation.new(
+			      		task_id: @task.id,
+			      		order_route_id: route.id,
+			      		status: "init"
+			      		)
+			      	@tor.save!
+			      	route.status="scheduled"
+			      	route.save!
+			      end
+			    end
+		    end
         flash[:success] = "Task Created Successful"
         redirect_to order_routes_path
       rescue => e
