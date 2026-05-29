a46c696888583576adb1100139b8a327a1fce653
lifuyuan <lifuyuan33@gmail.com>
2026-05-29 15:43:27 +0800

    n

---
+++ app/helpers/violation_appeals_helper.rb
@@ -0,0 +1,2 @@
+module ViolationAppealsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,8 @@ Rails.application.routes.draw do
   resources :order_routes
 
   resources :transport_tasks
+
+  resources :violation_appeals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
